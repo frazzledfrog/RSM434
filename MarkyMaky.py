18c0b06e5342e7edea5aadade0c961753eb20e38
frazzledfrog <alexandermckeown04@gmail.com>
2025-02-16 19:29:32 -0500

    Update MarkyMaky.py

--- MarkyMaky.py
+++ MarkyMaky.py
@@ -14,6 +14,7 @@ class Stock():
         self.rebate = rebate
         self.bid = None
         self.ask = None
+        self.spread = None
         self.position = 0
 
     def GetBidAsk(self): #update the bid and ask of the ticker
@@ -30,6 +31,7 @@ class Stock():
             
             self.bid_price = bid_prices_book[0]
             self.ask_price = ask_prices_book[0]
+            self.spread = self.ask - self.bid
 
     def GetPosition(self): #updates the position of the stock
             payload = {'ticker': self.ticker}
@@ -38,19 +40,27 @@ class Stock():
                 book = resp.json()
                 self.net_position = book[0]["position"]
 
-class Inventory():
+class Inventory(): #the inventory object is merely a way to do operations to a group of Stock objects
     def __init__(self, tickers):
         self.tickers = tickers
         self.gross_position = 0
         self.net_position = 0
+        self.weights = [0, 0, 0]
     
-    def update(self):
-        self.gross_position = 0
+    def Update(self): #also does an update to the position and bid-ask of each ticker
+        self.gross_position = 0 #reset to zero
         self.net_position = 0
+        #update the information
         for item in self.tickers:
             item.GetPosition()
+            item.GetBidAsk()
             self.gross_position += abs(item.position)
             self.net_position += item.position
+    
+    def target(self): #we will target the best performing of the tickers, accept an array as an input
+        max_spread = max(self.tickers, key = lambda stock: stock.spread)
+        return max_spread.ticker
+
 
 #functions
 def check_resp(resp, action): #function to call which checks whether a resp returned a valid code, and if it doesn't, prints the error code
@@ -59,3 +69,4 @@ def check_resp(resp, action): #function to call which checks whether a resp retu
     else:
         print("{} failed with code {}".format(action, resp.status_code))
         return False
+
